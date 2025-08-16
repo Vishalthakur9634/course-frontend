7d0a52291cea8ecf369f958042c81d2deba1dacd
Vishal <vishalthakur732007@gmail.com>
2025-08-16 21:35:19 +0530

    Updated frontend changes

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import { Link, useNavigate, useLocation } from 'react-router-dom';
 import { useAuth } from '../Context/AuthContext';
-import '../Styles/auth.css';
+import '../Styles/Auth.css';
 
 const Login = () => {
   const [formData, setFormData] = useState({
